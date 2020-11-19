db04c5ef9ce4cec31d96d2fc2b1b1c8ba1bf03b7
Yan Li <eyeplum@gmail.com>
2020-11-19 21:16:52 +1300

    Update cicero-tui to v0.1.3

--- Formula/cicero-tui.rb
+++ Formula/cicero-tui.rb
@@ -1,8 +1,8 @@
 class CiceroTui < Formula
   desc "Unicode tool with a terminal user interface"
   homepage "https://github.com/eyeplum/cicero-tui"
-  url "https://github.com/eyeplum/cicero-tui/archive/v0.1.2.tar.gz"
-  sha256 "dd04b7b2169a785495344f982ce24d3e046194c6fff7c8a84a41df76437bb663"
+  url "https://github.com/eyeplum/cicero-tui/archive/v0.1.3.tar.gz"
+  sha256 "2a81e91296e6cd02ad4b37d60a26bd1925826abf003bf88db2346d89297e0046"
   license "GPL-3.0-or-later"
   head "https://github.com/eyeplum/cicero-tui.git"
 
